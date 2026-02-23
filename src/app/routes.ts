1e15c1e0fcd2b51bba6fa29f66f0a0397c435b53
addhyan94 <addhyant85@gmail.com>
2026-02-24 01:08:03 +0530

    Party

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,10 @@
-import { createBrowserRouter } from "react-router";
+// import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router-dom";
 import { InvitationPage } from "./components/InvitationPage";
 import { AdminPanel } from "./components/AdminPanel";
 
-export const router = createBrowserRouter([
+// export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     Component: InvitationPage,
